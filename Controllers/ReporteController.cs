702e5d533cf6421fa4d9832b9e2d5474a9fff874
EMMANUEL JAIME VALENZUELA GONZALEZ <emmanuel.valenzuela02@inacapmail.cl>
2022-02-18 17:36:01 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/ReporteController.cs
@@ -0,0 +1,53 @@
+﻿using CapaDatos;
+using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VentasWeb.Controllers
+{
+    public class ReporteController : Controller
+    {
+        // GET: Reporte
+        public ActionResult Producto()
+        {
+            return View();
+        }
+
+        // GET: Reporte
+        public ActionResult Ventas()
+        {
+            return View();
+        }
+        public ActionResult Proveedore()
+        {
+            return View();
+        }
+
+        public JsonResult ObtenerProducto(int idtienda, string codigoproducto)
+        {
+            List<ReporteProducto> lista = CD_Reportes.Instancia.ReporteProductoTienda(idtienda, codigoproducto);
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult ObtenerVenta(string fechainicio, string fechafin, int idtienda, string tipodedocumento)
+        {
+            
+            List<ReporteVenta> lista = CD_Reportes.Instancia.ReporteVenta(Convert.ToDateTime(fechainicio), Convert.ToDateTime(fechafin), idtienda,tipodedocumento);
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ObtenerProveedor(string fechainicio, string fechafin, int idproveedor, int idtienda)
+        {
+            List<Compra> lista = CD_Compra.Instancia.ObtenerListaCompra(Convert.ToDateTime(fechainicio), Convert.ToDateTime(fechafin), idproveedor, idtienda);
+            return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
